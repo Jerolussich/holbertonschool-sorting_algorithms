f53c11fd1c6299e53a553ce420176a8e1cc94099
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-02 16:12:27 -0300

    Task 0 added

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+#include "print_list.c"
+#include "print_array.c"
+
+void bubble_sort(int *array, size_t size)
+{
+    int i = 0, temp = 0, cicle = 0, change = 0;
+
+    while (cicle != 1)
+    {
+        change = 0;
+        for (i = 0; i <= size; i++)
+        {
+            if (array[i] > array[i + 1] && i < size)
+            {
+                temp = array[i + 1];
+                array[i + 1] = array[i];
+                array[i] = temp;
+                change += 1;
+                print_array(array, size);
+            }
+        }
+        if (change == 0)
+            cicle = 1;
+    }
+}
