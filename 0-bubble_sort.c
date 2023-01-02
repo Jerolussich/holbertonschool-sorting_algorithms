33d7d9f44d5f634a82b6252e60218dc8f89c1989
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-02 18:09:54 -0300

    hotfix borderline cases

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,23 +7,31 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i = 0, temp = 0, cicle = 0, change = 0;
+	unsigned int i = 0, temp = 0, cycle = 0, change = 0;
 
-    while (cicle != 1)
-    {
-        change = 0;
-        for (i = 0; i <= size; i++)
-        {
-            if (array[i] > array[i + 1] && i < size)
-            {
-                temp = array[i + 1];
-                array[i + 1] = array[i];
-                array[i] = temp;
-                change += 1;
-                print_array(array, size);
-            }
-        }
-        if (change == 0)
-            cicle = 1;
-    }
+	if (size == 1)
+	{
+		printf("%d\n", array[0]);
+		return;
+	}
+	while (cycle != 1)
+	{
+		change = 0;
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				if ((i + 1) < size)
+				{
+					temp = array[i + 1];
+					array[i + 1] = array[i];
+					array[i] = temp;
+					change += 1;
+					print_array(array, size);
+				}
+			}
+		}
+		if (change == 0)
+			cycle = 1;
+	}
 }
