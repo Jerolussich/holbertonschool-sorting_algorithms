32a64691ebd48b8f3a2bee97f95ceabe66cbe290
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-04 12:38:22 -0300

    betty fixes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,39 +1,40 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sort a doubly linked list of integers in ascending order
+ * insertion_sort_list - sort a doubly linked list of integers
+ * in ascending order
  * using the Insertion sort algorithm
  * @list: pointer to a pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current;
+	listint_t *current;
 
-    if (list == NULL || *list == NULL)
-        return;
-    current = (*list)->next;
-    while (current != NULL)
-    {
-        /* Save the current node*/
-        listint_t *node = current;
-        
-        /*Find the insertion point*/
-        while (node->prev != NULL && node->prev->n > node->n)
-        {
-            /*Swap the node with its predecessor*/
-            node->prev->next = node->next;
-            if (node->next != NULL)
-                node->next->prev = node->prev;
-            node->next = node->prev;
-            node->prev = node->prev->prev;
-            node->next->prev = node;
-            if (node->prev != NULL)
-                node->prev->next = node;
-            else
-                *list = node;
-             /*Print the list*/
-             print_list(*list);
-        }
-        current = current->next;
-    }
+	if (list == NULL || *list == NULL)
+		return;
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		/* Save the current node*/
+		listint_t *node = current;
+
+		/*Find the insertion point*/
+		while (node->prev != NULL && node->prev->n > node->n)
+		{
+			/*Swap the node with its predecessor*/
+			node->prev->next = node->next;
+			if (node->next != NULL)
+				node->next->prev = node->prev;
+			node->next = node->prev;
+			node->prev = node->prev->prev;
+			node->next->prev = node;
+			if (node->prev != NULL)
+				node->prev->next = node;
+			else
+				*list = node;
+			/*Print the list*/
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
