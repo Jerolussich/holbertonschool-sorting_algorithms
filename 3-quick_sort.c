3604ea988bfe8e7c553bdc853fb0e871eb8e44bd
Jerolussich <109871263+Jerolussich@users.noreply.github.com>
2023-01-05 16:41:26 -0300

    Betty adjustment

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,14 +7,14 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	int i = -1, temp;
+	int i = -1, temp, pivot;
 	unsigned int j;
 
 	if (size < 2 || array == NULL)
 	{
 		return;
 	}
-	int pivot = array[size - 1];
+	pivot = array[size - 1];
 	for (j = 0; j < size - 1; j++)
 	{
 		if (array[j] < pivot)
