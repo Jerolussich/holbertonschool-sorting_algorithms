75384569d0bb42090037509bb1b30f49897d9801
root <brunocarnales@gmail.com>
2023-01-06 09:28:20 -0800

    hotfix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,15 +7,13 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	int i = -1, temp, pivot;
+	int i = -1, temp, pivot = array[size - 1];
 	unsigned int j;
 
 	if (size < 2 || array == NULL)
 	{
 		return;
 	}
-
-	pivot = array[size - 1];
 	for (j = 0; j < size - 1; j++)
 	{
 		if (array[j] < pivot)
@@ -28,10 +26,10 @@ void quick_sort(int *array, size_t size)
 		}
 	}
 
-	temp = array[i + 1];
-	array[i + 1] = pivot;
+	temp = array[i];
+	array[i] = pivot;
 	array[size - 1] = temp;
 	print_array(array, size);
-	quick_sort(array, i + 1);
-	quick_sort(array + i + 2, size - i - 2);
+	quick_sort(array, i);
+	quick_sort(array + i + 1, size - i - 1);
 }
